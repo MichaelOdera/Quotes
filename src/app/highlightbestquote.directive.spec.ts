d597a5aa8444ab8d3a1e3c906f515628b7674c0f
MichaelOdera <michogelira@gmail.com>
2020-01-12 19:02:27 +0300

    create directive to highligt best quote

---
+++ src/app/highlightbestquote.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighlightbestquoteDirective } from './highlightbestquote.directive';
+
+describe('HighlightbestquoteDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlightbestquoteDirective();
+    expect(directive).toBeTruthy();
+  });
+});
