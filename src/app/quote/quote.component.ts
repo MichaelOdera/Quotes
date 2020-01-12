9d9fc25652f153f18a6cec2ab9e8f110f463d935
MichaelOdera <michogelira@gmail.com>
2020-01-12 19:05:11 +0300

    add border to the quote div

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,6 +13,7 @@ export class QuoteComponent implements OnInit {
   bestQuote:string;
   bestAuthor:string;
   bestType:string;
+  downVote:number;
 
   //Initialise the variables for the upvotes and the downvotes
   upvote = 0;
@@ -23,9 +24,27 @@ export class QuoteComponent implements OnInit {
   quotes:Quote[]=[
     new Quote("Inspirational", "I am happy about the current economy", "Mildred kins",0,0,),
     new Quote("Scientific", "Science Changes the world", "Potiphar Clarks",0,0,),
+    new Quote("Scientific", "Science Changes the world", "Potiphar Clarks",0,0,),
   ];
 
 
+  showBestQuote(){
+    this.mostLikes=0;
+    for(let i=0; i<this.quotes.length; i++){
+      if(this.quotes[i].upvote>this.mostLikes){
+        this.bestType = this.quotes[i].type;
+        this.bestQuote = this.quotes[i].content;
+        this.mostLikes = this.quotes[i].upvote;
+        this.bestAuthor = this.quotes[i].author;
+        this.downVote = this.quotes[i].downvote;
+
+      }
+    }
+  }
+
+
+
+  //the downvote and upvote function section
   thumbsUp(quote){
     quote.upvote = quote.upvote + 1;
   }
