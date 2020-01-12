e02f881e8941229381bf15db7c5ceb7561745e63
MichaelOdera <michogelira@gmail.com>
2020-01-12 19:02:55 +0300

    set color of directive to 	#ffb39d

---
+++ src/app/highlightbestquote.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlightbestquote]'
+})
+export class HighlightbestquoteDirective {
+
+  constructor(private elem:ElementRef) { 
+    this.elem.nativeElement.style.backgroundColor = '	#ffb39d';
+  }
+
+}
